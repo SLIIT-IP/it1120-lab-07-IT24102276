9b38d366b4a32556de2375d73f8f54f4ac70d19b
IT24102276 <it24102276@my.sliit.lk>
2024-09-24 12:23:24 +0100

    Add files via upload

---
+++ Q2/IT24102276Lab7Q2A.java
@@ -0,0 +1,14 @@
+public class IT24102276Lab7Q2A{
+public static void main(String[] args){
+
+for(int i = 1; i <= 5; i++)
+{
+for(int j = 1; j <= 4; j++)
+{
+System.out.print("$ ");
+}
+System.out.println("$ ");
+}
+}
+}
+
